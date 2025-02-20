e34031f7cd273e88105cdb7a4e9345d399f6002a
afinayasyifa24 <yasyifaafina@gmail.com>
2025-02-20 13:15:38 +0700

    praktikum01

--- README.md
+++
@@ -1,7 +0,0 @@
-# pemograman web 2
-
-## repository praktikum pemograman web 2
-
-- Nama : afina yasyifa 
-- NIM : 0110124255
-- Rombel : SI 10
---
+++ variabel_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendefinikan variabel
+$nama = 'apinaa';
+$umur = 19;
+$berat = 45.05;
+
+echo 'nama saya ' .  $nama;
+echo '<br/>umur : ' . $umur . 'tahun' ;
+echo '<br/>berat : ' . $berat;
+
+?>
